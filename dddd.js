e63274152f2f7cf6f3c04b3c0e11ef6292c76619
Max33232 <134615167+Max33232@users.noreply.github.com>
2026-08-27 01:08:47 +0700

    asdf

--- dddd.js
+++ dddd.js
@@ -77949,15 +77949,15 @@
   }
 
   (function SADSAAWaitGui() {
-    if (!document.body && !document.documentElement) { setTimeout(SADSAAWaitGui, 100); return; }
-    SADSAALoadDatGui(function () { setTimeout(SADSAABuildMenu, 800); });
+    if (!document.body) { setTimeout(SADSAAWaitGui, 100); return; }
+    SADSAALoadDatGui(function () { setTimeout(SADSAABuildMenu, 3000); });
   })();
 
   window.SADSAA_MOD = SADSAA_MOD;
   // ========== END SADSAA MOD ==========
   
   function ࡀ٢ε() {
-    ܐ༠ܚ = true;
+    ܐ༠ܚ = Іߓއ && document.body !== null && document.querySelector("canvas") !== null;
     if (ܐ༠ܚ === true) {
       ⲣߌߋ.Ꮷ̶５();
       ࡀܒ༠.Ꮷ̶５();
